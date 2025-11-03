877e7fee7ab8d852da3e53d2ae3cac888864d022
qneolabs <quantumneolabs@gmail.com>
2025-11-03 19:49:40 +0700

    Create Home component in index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+
+return (
+
+<main>
+
+<h1>qneolabs</h1>
+
+<p>Modern sites. Measurable results.</p>
+
+<p><a href="[mailto:quantumneolabs@gmail.com">Email](mailto:quantumneolabs@gmail.com">Email) us</a></p>
+
+</main>
+
+);
+
+}
